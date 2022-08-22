1fe9ea0927ed79bcfbd1079ac71e7dca692e26e0
idoasisay <ihannah@kakao.com>
2022-08-22 12:53:57 +0900

    폴더 구성

---
+++ consumer/client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div className='App'>
+      <Typography variant='h1' gutterBottom>
+        h1. Heading
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
