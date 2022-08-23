1f03d3b266aee8efe17377bbc63c076f5234b2af
idoasisay <ihannah@kakao.com>
2022-08-23 16:15:05 +0900

    푸터, 헤더 업데이트 및 유저 데이터 관리 삽입

---
+++ client/src/components/Header.js
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import '../assets/css/header.css';
+
+import { Link } from 'react-router-dom';
+
+import { userState } from '../recoil/user';
+import { useRecoilValue } from 'recoil';
+// import LogoutButton from './LogoutButton';
+
+import { isLoginUser } from '../utils/user';
+
+export default function Header() {
+  const user = useRecoilValue(userState);
+  return (
+    <header>
+      <div className='header_container'>
+        <Link to='/' className='header_logo'>
+          <img src='signature_horizontal_color.png' alt='코드스테이츠 로고' />
+        </Link>
+        <ul className='header_menu'>
+          <li className='header_logout'>
+            {isLoginUser(user) ? (
+              <Link to='/' className='logoutButton'>
+                로그아웃
+              </Link>
+            ) : null}
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
