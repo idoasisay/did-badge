41e672ad8cf024a04107007af5923078894d709b
idoasisay <ihannah@kakao.com>
2022-08-25 11:05:25 +0900

    index 라우터 생성

--- server/app.js
+++ server/app.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const indexRoute = require('./routes/index');
+const { sequelize } = require('./models/index');
 
 const app = express();
 const port = 4000;
 
+// cors 처리
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+// 응답 확인
 app.use(
   morgan('      :method :url :status :res[content-length] - :response-time ms')
 );
 
+// 파싱
 app.use(express.json());
 
+// 라우터 분기
+app.use('/', indexRoute);
+
+// 서버 생성
 const server = app.listen(port, () => {
   console.log(`✅ ${port} server is ok`);
 });
+
+// DB
+sequelize
+  .sync({ force: false })
+  .then(() => console.log(`✅ DB is ok`))
+  .catch(err => console.error(err));
 
 module.exports = server;
